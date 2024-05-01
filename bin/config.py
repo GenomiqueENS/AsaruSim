de5353b2c614e53989648ac99ee7467be328af42
Ali Hamraoui <hamraoui@bio.ens.psl.eu>
2024-05-01 18:52:11 +0200

    RM: remove config.py

--- bin/config.py
+++
@@ -1,4 +0,0 @@
-# constant sequence
-ADPTER_SEQ='ACTAAAGGCCATTACGGCCTACACGACGCTCTTCCGATCT'
-TSO_SEQ='TGTACTCTGCGTTGATACCACTGCTT'
-# '/home/ubuntu/vol_data/project/SC_analysis/data/Genome_n_Anno/gencode.v31.transcripts.fa'
