c147a7f66d094df08f12e2f52b1230536e9ff977
Ali Hamraoui <hamraoui@bio.ens.psl.eu>
2024-05-19 12:57:23 +0200

    UPDATE: limit number of sequences to control quality in 100K sequences

--- bin/libs/qc_generator.py
+++ bin/libs/qc_generator.py
@@ -10,9 +10,11 @@ from fqread.fastq_reader import SequenceStats
 
 def process_fastq_file(filename, pos, reverse):
     stats = SequenceStats(pos)
+    sequence_count = 0 
+    
     open_fn = gzip.open if filename[0].endswith('.gz') else open
     with open_fn(filename[0], 'rb') as f:
-        while True:
+        while sequence_count < 100000:
             header = f.readline()
             if not header: break  # EOF
             if reverse:
@@ -25,6 +27,7 @@ def process_fastq_file(filename, pos, reverse):
                 quality = f.readline().strip()
             
             stats.add_sequence(seq, quality)
+            sequence_count += 1
 
     return stats.calculate_stats()
 
